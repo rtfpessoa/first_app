4029ec571cb98582d4ac9b35b6ae0b5a3838cd9a
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2013-06-06 22:56:24 +0100

    Blog initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 FirstApp::Application.routes.draw do
+  resources :posts
+
+
+  # get "home/index"
+  
+  #...
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  root :to => "home#index"  
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
