6a54ea335445baf321be5bb813f767caf996201d
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2013-06-07 01:24:43 +0100

    Add tags to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-    attr_accessible :content, :name, :title
+    attr_accessible :content, :name, :title, :tags_attributes
 
     validates :name, :presence => true,
                      :length => { :minimum => 5 }
@@ -8,5 +8,9 @@ class Post < ActiveRecord::Base
     validates :content, :presence => true
 
     has_many :comments, :dependent => :destroy
+    has_many :tags
+    
+    accepts_nested_attributes_for :tags, :allow_destroy => :true,
+        :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }    
 end
 
